3973d439a4e773fd97e17c127d1f53641ecc4b52
ANA CAROLINA LEANDRO <anaaacarolina5@gmail.com>
2026-03-09 00:54:25 -0300

    Update UsuarioRepository.java
    Implementação dos métodos declarados na interface IUsuarioRepository para operações realizadas no ArrayList.

--- src/repository/UsuarioRepository.java
+++ src/repository/UsuarioRepository.java
@@ -1,5 +1,35 @@
 package repository;
 
-public class UsuarioRepository {
+import model.Usuario;
+import java.util.ArrayList;
+
+public class UsuarioRepository implements IUsuarioRepository {
+
+    private ArrayList<Usuario> arraylist;
+
+    public UsuarioRepository(){
+        arraylist = new ArrayList<Usuario>();
+    }
+
+    @Override
+    public void adicionar(Usuario usuario){
+        arraylist.add(usuario);
+    }
+
+    @Override
+    public void remover(Usuario usuario){
+        int i = arraylist.indexOf(usuario);
+        if (i != -1){
+            arraylist.remove(i);
+        }
+    }
+
+    @Override
+    public void editar(Usuario usuario){
+        int i = arraylist.indexOf(usuario);
+        if(i != -1){
+            arraylist.set(i, usuario);
+        }
+    }
 
 }
