5eabceda089de4d4a14913e71e98fe50cf9d5e87
ANA CAROLINA LEANDRO <anaaacarolina5@gmail.com>
2026-03-10 02:06:24 -0300

    Update UsuarioController.java
    Implementação dos métodos para receber as chamadas da interface do usuário, dialogar com service e retornar resultados.

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -1,5 +1,35 @@
 package controller;
 
+import model.Usuario;
+import service.CpfInvalidoException;
+import service.DataInvalidaException;
+import service.UsuarioJaCadastradoException;
+import service.UsuarioNaoCadastradoException;
+import service.UsuarioService;
+
 public class UsuarioController {
+
+    private UsuarioService service;
+
+    public UsuarioController(UsuarioService service){
+        this.service = service;
+    }
+
+    public void adicionar(Usuario usuario) 
+        throws UsuarioJaCadastradoException, CpfInvalidoException, DataInvalidaException {
+        service.adicionar(usuario);
+    }
+
+    public void remover(String cpf) throws UsuarioNaoCadastradoException {
+        service.remover(cpf);
+    }
+
+    public void editar(Usuario usuario) throws UsuarioNaoCadastradoException {
+        service.editar(usuario);
+    }
+
+    public Usuario consultar(String cpf) {
+        return service.consultar(cpf);
+    }
 
 }
