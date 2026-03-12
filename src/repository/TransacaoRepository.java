0ceb470a1c4e9bb77dc8aff9b98e6276398544a4
andré <araujoandre.pessoal@gmail.com>
2026-03-12 00:25:03 -0300

    Update TransacaoRepository.java

--- src/repository/TransacaoRepository.java
+++ src/repository/TransacaoRepository.java
@@ -1,27 +1,34 @@
 package repository;
 
 import model.Transacao;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TransacaoRepository implements ITransacaoRepository {
 
-    private List<Transacao> banco = new ArrayList<>();
+    private Map<String, List<Transacao>> banco = new HashMap<>();
 
     @Override
-    public void salvar(Transacao t){
-        banco.add(t);
+    public void salvar(String cpfUsuario, Transacao t) {
+        banco.putIfAbsent(cpfUsuario, new ArrayList<>());
+        banco.get(cpfUsuario).add(t);
     }
 
     @Override
-    public void remover(int id){
-        if(id >= 0 && id < banco.size()){
-            banco.remove(id);
+    public void remover(String cpfUsuario, int indice) {
+        List<Transacao> transacoes = banco.get(cpfUsuario);
+
+        if (transacoes != null && indice >= 0 && indice < transacoes.size()) {
+            transacoes.remove(indice);
         }
     }
 
     @Override
-    public List<Transacao> listar(){
-        return banco;
+    public List<Transacao> listar(String cpfUsuario) {
+        banco.putIfAbsent(cpfUsuario, new ArrayList<>());
+        return banco.get(cpfUsuario);
     }
 }
