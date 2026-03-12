8b729edd3decd81d79025d6410bc5b8cebae308d
andré <araujoandre.pessoal@gmail.com>
2026-03-12 16:22:11 -0300

    Update ITransacaoRepository.java

--- src/repository/ITransacaoRepository.java
+++ src/repository/ITransacaoRepository.java
@@ -3,9 +3,17 @@ package repository;
 import java.util.List;
 import model.Transacao;
 
+/**
+Interface responsável por definir as operações básicas
+de armazenamento e recuperação de transações financeiras.
+
+Permite que diferentes implementações de repositório sejam
+utilizadas sem impactar as outras camadas do sistema. @André */
+
 public interface ITransacaoRepository {
 
     void salvar(String cpfUsuario, Transacao t);
     void remover(String cpfUsuario, int indice);
     List<Transacao> listar(String cpfUsuario);
 }
+
