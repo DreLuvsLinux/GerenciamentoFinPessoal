ba0847fd98978233924e7b49c53a417d3c0a79ee
andré <araujoandre.pessoal@gmail.com>
2026-03-12 00:28:01 -0300

    Update LoginView.java

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,6 +1,8 @@
 package view;
 
 import java.util.Scanner;
+
+import controller.TransacaoController;
 import controller.UsuarioController;
 import model.Usuario;
 import service.exceptions.UsuarioNaoCadastradoException; // adicionando o lançamento caso usucario nao seja encontrado
@@ -8,29 +10,29 @@ import service.exceptions.UsuarioNaoCadastradoException; // adicionando o lança
 public class LoginView {
 
     private UsuarioController controller;
+    private TransacaoController transacaoController;
     private Scanner scanner;
 
-    public LoginView(UsuarioController controller) {
+    public LoginView(UsuarioController controller, TransacaoController transacaoController) {
         this.controller = controller;
-        scanner = new Scanner(System.in);
+        this.transacaoController = transacaoController;
+        this.scanner = new Scanner(System.in);
     }
 
     public void fazerLogin() {
 
         System.out.print("Digite o CPF: ");
-        String cpf = scanner.nextLine(); 
+        String cpf = scanner.nextLine();
 
         try {
-            
-            Usuario usuario = controller.consultar(cpf);
+            Usuario usuario = controller.login(cpf);
 
             System.out.println("Login realizado com sucesso!");
 
-            UsuarioView usuarioView = new UsuarioView(controller, usuario);
+            UsuarioView usuarioView = new UsuarioView(controller, usuario, transacaoController);
             usuarioView.menuUsuario();
 
         } catch (UsuarioNaoCadastradoException e) {
-
             System.out.println("Usuário não encontrado.");
         }
     }
