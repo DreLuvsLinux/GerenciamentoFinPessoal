bf43eff7be164436204c35ee71e2e1ed69850819
andré <araujoandre.pessoal@gmail.com>
2026-03-11 22:57:03 -0300

    Update Relatorio.java

--- src/model/relatorio/Relatorio.java
+++ src/model/relatorio/Relatorio.java
@@ -1,1 +1,9 @@
+package model.relatorio;
 
+import model.Usuario;
+
+public abstract class Relatorio { // ABSTRAÇÃO !! a abstração foi utilizada na classe aberta ***Relatorio***
+    
+    public abstract void gerar(Usuario usuario);
+
+}
