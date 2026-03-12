8976ab3f72ef82ab62b9e50595b02dc10886d7e0
andré <araujoandre.pessoal@gmail.com>
2026-03-12 16:16:51 -0300

    exclusão comentário (desnecessário) RelatorioDespesas.java

--- src/model/relatorio/RelatorioDespesas.java
+++ src/model/relatorio/RelatorioDespesas.java
@@ -4,16 +4,6 @@ import controller.TransacaoController;
 import model.Transacao;
 import model.Usuario;
 
-/**
-Implementação de relatório responsável por listar
-todas as receitas registradas pelo usuário.
-
-O relatório percorre as transações do usuário e exibe
-apenas aquelas classificadas como receita.
-
-Esta classe demonstra o uso de herança ao estender
-a classe abstrata Relatorio. @João Pedro */
-
 public class RelatorioDespesas extends Relatorio { // HERANÇA!!!!
 
     private TransacaoController controller;
@@ -35,3 +25,4 @@ public class RelatorioDespesas extends Relatorio { // HERANÇA!!!!
     }
 
 }
+
