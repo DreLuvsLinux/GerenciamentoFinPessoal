1e3e57203f8adedfdde0aff17eeb43303acb245d
andré <araujoandre.pessoal@gmail.com>
2026-03-12 00:22:17 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,1 +1,13 @@
+package main;
 
+import view.TelaInicialView;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TelaInicialView telaInicial = new TelaInicialView();
+        telaInicial.iniciar();
+
+    }
+}
